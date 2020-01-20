cc7d6c0701fa9dffbf7ef62a1e81ec2950257273
Faizan Ansari <faizanansari@Faizans-MacBook-Pro.local>
2020-01-20 15:25:04 -0600

    Final submission to github

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+﻿//checks off the items on the list
+$('ul').on('click','li', function () {
+    $(this).toggleClass('completed');
+});
+//click to delete function and showcase bubble effect
+$('ul').on('click','span', function (e) {
+    $(this).fadeOut(500,function () {
+        $(this).parent().remove();
+        e.stopPropagation();
+    })
+    
+})
+$('input[type="text"]').keypress(function (e) {
+    if (e.which == 13) {
+        var userInput = $(this).val();
+        $('ul').append('<li>' + '<span><i class="fa fa-ban"></i></span> '+ userInput + '</li>');
+        $(this).val('');
+
+    }
+
+})
+$(".fa-plus").click(function () {
+    $('input[type="text"]').fadeToggle();
+})
